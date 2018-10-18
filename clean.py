6f742b103725493d39a2e4f8b639178e75a67231
Michael Doronin <warrior2031@mail.ru>
2018-10-18 23:40:15 +0300

    small refactoring

--- clean.py
+++ clean.py
@@ -5,14 +5,14 @@ from difflib import SequenceMatcher
 from subprocess import check_output
 
 
-_, file, pattern = sys.argv
+file, pattern = sys.argv[1], bytes(sys.argv[2], 'utf8')
 index_version = check_output(['git', 'show', f':{file}'])
 with open(file, 'rb+') as f:
     current = bytearray(f.read())
-    m = SequenceMatcher(a=index_version, b=current)
-    for tag, i1, i2, j1, j2 in reversed(m.get_opcodes()):
+    matcher = SequenceMatcher(a=index_version, b=current)
+    for tag, i1, i2, j1, j2 in reversed(matcher.get_opcodes()):
         if tag in {'insert', 'replace'}:
-            for m in re.finditer(bytes(pattern, 'utf8'), current[j1:j2]):
+            for m in re.finditer(pattern, current[j1:j2]):
                 del current[j1 + m.start():j1 + m.end()]
     f.seek(0)
     f.write(current)
